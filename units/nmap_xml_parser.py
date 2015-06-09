9c2f49e7ef7176ef6185abd545f1c0baf05efcb5
Bryan T. Richardson <bryan@activeshadow.com>
2015-06-09 06:57:52 -0400

    Nmap Parser transform now handles multiple files
    You can now simply provide a directory to the Nmap XML Parser transform
    and it will recursively search for XML result files.
    
    The only requirement is that the XML files be named `results.xml`.

--- units/nmap_xml_parser.py
+++ units/nmap_xml_parser.py
@@ -1,3 +1,5 @@
+import fnmatch, os
+
 from MaltegoTransform import *
 from lxml             import etree
 
@@ -5,36 +7,47 @@ m = MaltegoTransform()
 m.parseArguments(sys.argv)
 
 filename = m.getValue()
-document = etree.parse(filename)
 
-for host in document.xpath("//host[ports/port[state[@state='open']]]"):
-  iface = None
+results = []
 
-  for addr in host.xpath("address[@addrtype='ipv4']"):
-    iface = m.addEntity("maltego.IPv4Address", addr.attrib['addr'])
+if os.path.isdir(filename):
+  for root, dirnames, filenames in os.walk(filename):
+    for result in fnmatch.filter(filenames, 'results.xml'):
+      results.append(os.path.join(root, result))
+else:
+  results.append(filename)
 
-  if iface != None:
-    ports = []
+for result in results:
+  document = etree.parse(result)
 
-    for port in host.xpath("ports/port[state[@state='open']]"):
-      pnum = port.attrib['portid']
-      name = []
+  for host in document.xpath("//host[ports/port[state[@state='open']]]"):
+    iface = None
 
-      try:
-        svc = port.xpath("service")[0]
+    for addr in host.xpath("address[@addrtype='ipv4']"):
+      iface = m.addEntity("maltego.IPv4Address", addr.attrib['addr'])
+
+    if iface != None:
+      ports = []
+
+      for port in host.xpath("ports/port[state[@state='open']]"):
+        pnum = port.attrib['portid']
+        name = []
 
         try:
-          name.append(svc.attrib['product'])
-          name.append(svc.attrib['version'])
-          name.append(svc.attrib['extrainfo'])
-        except KeyError:
-          if len(' '.join(name)) == 0:
-            name.append(svc.attrib['name'])
-      except:
-        pass
+          svc = port.xpath("service")[0]
 
-      ports.append("%s::%s" % (pnum, ' '.join(name)))
+          try:
+            name.append(svc.attrib['product'])
+            name.append(svc.attrib['version'])
+            name.append(svc.attrib['extrainfo'])
+          except KeyError:
+            if len(' '.join(name)) == 0:
+              name.append(svc.attrib['name'])
+        except:
+          pass
 
-    iface.addAdditionalFields("service.ports", "Listening Ports", "strict", '|'.join(ports))
+        ports.append("%s::%s" % (pnum, ' '.join(name)))
+
+      iface.addAdditionalFields("service.ports", "Listening Ports", "strict", '|'.join(ports))
 
 m.returnOutput()
